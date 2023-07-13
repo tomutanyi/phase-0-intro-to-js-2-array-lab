e23950132796e6961edb77c7d685d1c84a696797
tomutanyi <tomutanyi@gmail.com>
2023-07-13 10:05:03 +0300

    Completed assignment

---
+++ array.js
@@ -0,0 +1,13 @@
+const cats = ["Milo", "Otis", "Garfield"];
+
+console.log(cats)
+
+
+function removeLastCat(){
+    const last = cats.pop()
+    return last
+}
+
+function removeLastCat()
+
+console.log(cats)
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop(cats);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift(cats);
+}
+
+function appendCat(cat){
+    return [...cats, cat]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    const cat4 = cats.slice(0, 2)
+    return cat4
+}
+
+function removeFirstCat(){
+    const cat4 = cats.slice(1)
+    return cat4
+}
